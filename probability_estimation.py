43639a442c19bd32d119afa5de31c5b865133074
sudarshanseshadri <sudarshadri@gmail.com>
2017-11-02 17:15:07 -0700

    probability_estimatio.py runs without fatal errors

--- probability_estimation.py
+++ probability_estimation.py
@@ -14,7 +14,7 @@ G = snap.LoadEdgeList(snap.PUNGraph, socialGraphFilename)
 Au = defaultdict(int)
 Av2u = defaultdict(int)
 Avnu = defaultdict(int)
-tau = defaultdict(tuple)
+tau = defaultdict(lambda : (0., 0))
 credit = {}
 
 curr_action = data[0][location_id_idx]
@@ -38,17 +38,14 @@ for row in data:
 	for previous_row in current_table:
 		previous_user, previous_action, previous_timestamp = previous_row
 		if G.IsEdge(int(user), int(previous_user)):
-			# if timestamp > previous_timestamp:
-			Av2u[(previous_user, user)] += 1
-			parents.add(previous_user)
-			# Instead of average, we were lazy and just did max.
-			val, count = tau[(previous_user, user)]
-			tau[(previous_user, user)] = (float((val * count) + (timestamp - previous_timestamp)) / (count + 1), count + 1)
+			if (timestamp - previous_timestamp).days > 0:
+				Av2u[(previous_user, user)] += 1
+				parents.add(previous_user)
+				# Instead of average, we were lazy and just did max.
+				val, count = tau[(previous_user, user)]
+				tau[(previous_user, user)] = (float((val * count) + (timestamp - previous_timestamp).days) / (count + 1), count + 1)
 			Avnu[(previous_user,user)] += 1
 	for p in parents:
 		credit[(p, user, curr_action)] = 1.0/len(parents)
 	current_table.add((user, action, timestamp))
-
-# Convert tau dict to average
-tau = {(v,u):sum(tau[(v,u)]) / float(len(tau[(v,u)])) for v, u in tau}
 
