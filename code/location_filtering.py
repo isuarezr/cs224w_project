99703b3f8c3f2a0a0a567233f7d72a37245e7205
sudarshanseshadri <sudarshadri@gmail.com>
2017-12-06 14:27:01 -0800

    location_filtering now functional

--- code/location_filtering.py
+++ code/location_filtering.py
@@ -9,43 +9,51 @@ def get_distance_km(location_1, location_2):
 
 #return longitude, latitude where we think each user lives
 def get_user_locations(checkin_data_filename):
-	data = np.loadtxt(filename, dtype='str', usecols=[0,2,3], delimiter='\t')
+	print "Getting user locations"
+	data = np.loadtxt(checkin_data_filename, dtype='str', usecols=[0,2,3], delimiter='\t')
+	print "Checkins loaded"
 	user_to_locations = defaultdict(list)
 	for user, latitude, longitude in data:
 		user = int(user)
-		latitude = float(latitude)
-		longitude = float(longitude)
-		user_to_locations.append((latitude, longitude))
+		try:
+			latitude = float(latitude)
+			longitude = float(longitude)
+		except:
+			continue
+		user_to_locations[user].append((latitude, longitude))
 	user_to_home = {}
+	num_iters = len(user_to_locations)
+	print num_iters
+	iters = 0
 	for user in user_to_locations:
-		user_to_home[user] = compute_user_location(user_locations[user])
+		user_to_home[user] = compute_user_location(user_to_locations[user])
+		iters += 1
+		if iters % 100 == 0:
+			print "Done with {} iters of {}".format(iters, num_iters)
 	return user_to_home
 
-def compute_user_location(user_locations):
-	def average_location(locations):
-		n = len(locations)
-		latitude = sum[locations[i][0] for i in range(n)] / n
-		longitude = sum[locations[i][1] for i in range(n)] / n
-		return (latitude, longitude)
-
-	n = len(user_locations)
-	for i in xrange(n/2):
-		centroid = average_location(user_locations)
-		furthest_idx = max([(get_distance_km(centroid, user_locations[i]), i) for i n range(len(user_locations))])[1]
-		user_locations.pop(furthest_idx)
-	return average_location(user_locations)
+def compute_user_location(locations):
+	n = len(locations)
+	latitude = sum([locations[i][0] for i in range(n)]) / n
+	longitude = sum([locations[i][1] for i in range(n)]) / n
+	return (latitude, longitude)
 
 #return a slice of G based on the query location
 def get_graph_slice(G, checkin_data_filename, query_location, max_distance):
-	user_locations = get_user_locations(checkin_data)
+	user_locations = get_user_locations(checkin_data_filename)
 	user_distance_pairs = [(user, get_distance_km(query_location, user_locations[user])) for user in user_locations]
 	far_users = [user for user, dist in user_distance_pairs if dist > max_distance]
 	V = snap.TIntV()
 	for user in far_users:
-		V.add(user)
+		V.Add(user)
 	G_prime = snap.ConvertGraph(type(G), G)
 	old_G_size = G.GetNodes()
 	snap.DelNodes(G_prime, V)
 	assert G.GetNodes() == old_G_size
 	return G_prime
 
+# G = snap.LoadEdgeList(snap.PUNGraph, '../data/Brightkite_edges.txt')
+# checkin_data_filename = '../data/Brightkite_totalCheckins.txt'
+# query_location = (39.05, -94.59)
+# max_distance = 50
+# G_prime = get_graph_slice(G, checkin_data_filename, query_location, max_distance)
