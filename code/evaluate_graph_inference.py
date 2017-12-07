bc7190f2a4fdfbe716c83db34a8a9024227fe514
sudarshanseshadri <sudarshadri@gmail.com>
2017-12-07 15:44:13 -0800

    Implemented function infer_graph

--- code/evaluate_graph_inference.py
+++ code/evaluate_graph_inference.py
@@ -4,4 +4,14 @@ def compare_true_versus_inferred(G, G_prime):
 	pass
 
 def infer_graph(weights, threshold):
-	pass
+	assert 0 <= threshold < 1
+	G = snap.TUNGraph.New()
+
+	for (u,v) in weights:
+		if not G.IsNode(u):
+			G.AddNode(u)
+		if not G.IsNode(v):
+			G.AddNode(v)
+		if weights[(u,v)] >= threshold and not G.IsEdge(u,v):
+			G.AddEdge(u,v)
+	return G
